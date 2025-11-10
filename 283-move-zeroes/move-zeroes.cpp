d374f7253739d0d99537eccb267ca539c232fbde
nidhi <nidhidevulapally20@gmail.com>
2025-11-10 11:41:59 +0530

    Time: 2 ms (17.96%) | Memory: 24 MB (20.36%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,9 +4,7 @@ public:
         int n1 = 0;
         for(int n2 = 0; n2 < nums.size(); n2++) {
             if(nums[n2]!=0) {
-                int temp = nums[n2]; 
-                nums[n2] = nums[n1];
-                nums[n1] = temp;
+                swap(nums[n1], nums[n2]); 
                 n1++;
             }
         }
