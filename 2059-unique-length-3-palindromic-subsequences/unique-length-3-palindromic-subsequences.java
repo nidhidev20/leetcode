ba8a4a850f822e7d2692abddae811d3021fec8f1
nidhi <nidhidevulapally20@gmail.com>
2025-11-21 19:26:15 +0530

    Time: 31 ms (98.48%) | Memory: 47.1 MB (13.68%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -1,33 +1,38 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        Set<Character> letters = new HashSet();
-        for (Character c: s.toCharArray()) {
-            letters.add(c);
+        int n = s.length();
+        int[] first = new int[26];
+        int[] last = new int[26];
+        Arrays.fill(first, n);
+        Arrays.fill(last, -1);
+
+        // find first & last
+        for (int i = 0; i < n; i++) {
+            int c = s.charAt(i) - 'a';
+            first[c] = Math.min(first[c], i);
+            last[c] = i;
         }
-        
-        int ans = 0;
-        for (Character letter : letters) {
-            int i = -1;
-            int j = 0;
+
+        int res = 0;
+
+        // for each character as the outer sides
+        for (int c = 0; c < 26; c++) {
+            int L = first[c];
+            int R = last[c];
+            if (R - L < 2) continue; // no space inside
             
-            for (int k = 0; k < s.length(); k++) {
-                if (s.charAt(k) == letter) {
-                    if (i == -1) {
-                        i = k;
-                    }
-                    
-                    j = k;
+            boolean[] memo = new boolean[26];
+
+            // scan only between L and R
+            for (int i = L + 1; i < R; i++) {
+                int idx = s.charAt(i) - 'a';
+                if (!memo[idx]) {
+                    memo[idx] = true;
+                    res++;
                 }
             }
-            
-            Set<Character> between = new HashSet();
-            for (int k = i + 1; k < j; k++) {
-                between.add(s.charAt(k));
-            }
-            
-            ans += between.size();
         }
-        
-        return ans;
+
+        return res;
     }
 }
