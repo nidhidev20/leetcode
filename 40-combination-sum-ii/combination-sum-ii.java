dacc7cf1cc35ae7e1a6d0088981fdf866f66a9a9
nidhi <nidhidevulapally20@gmail.com>
2025-12-07 14:39:13 +0530

    Time: 7 ms (22.68%) | Memory: 45.5 MB (9.44%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Set<List<Integer>> ans = new HashSet<>();
+        Arrays.sort(candidates);
+        find(0, candidates, target, ans, new ArrayList<>());
+        return new ArrayList<>(ans);
+    }
+
+    private void find(int idx, int[] arr, int target,  Set<List<Integer>> ans, List<Integer> ds) {
+        if(target == 0) {
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for(int i = idx; i < arr.length; i++) {
+            if(i > idx && arr[i] == arr[i-1]) continue;
+            if(arr[i] > target) break;
+
+            ds.add(arr[i]);
+
+            find(1+i, arr, target - arr[i], ans, ds);
+
+            ds.remove(ds.size() - 1);
+        }
+    }
+}
