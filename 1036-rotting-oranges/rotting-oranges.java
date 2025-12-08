836068690f663094ac4aca16dd9ca6cd8088e36b
nidhi <nidhidevulapally20@gmail.com>
2025-12-08 11:45:32 +0530

    Time: 2 ms (80.54%) | Memory: 43.6 MB (62.55%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+        int fresh = 0;
+
+        // collect initial rotten oranges and count fresh
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 2) {
+                    q.offer(new int[]{i, j});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        if (fresh == 0) return 0;
+
+        int cnt = 0;
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        while (!q.isEmpty()) {
+            cnt++;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int[] point = q.poll();
+                for (int[] dir : dirs) {
+                    int x = point[0] + dir[0];
+                    int y = point[1] + dir[1];
+
+                    if (x < 0 || y < 0 || x >= rows || y >= cols
+                            || grid[x][y] == 0 || grid[x][y] == 2) {
+                        continue;
+                    }
+                    grid[x][y] = 2;
+                    q.offer(new int[]{x, y});
+                    fresh--;
+                }
+            }
+        }
+
+        return fresh == 0 ? cnt - 1 : -1;
+    }
+}
