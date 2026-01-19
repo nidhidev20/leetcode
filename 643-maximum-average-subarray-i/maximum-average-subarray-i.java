d56fbc52e7fa7f30ef3ba04d5cfee85910f58a40
nidhi <nidhidevulapally20@gmail.com>
2026-01-19 18:37:04 +0530

    Time: 3 ms (84.70%) | Memory: 69.5 MB (69.25%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -3,12 +3,10 @@ class Solution {
         int max = Integer.MIN_VALUE, sum = 0, n = nums.length;
         for(int i = 0; i < k; i++) {
             sum += nums[i];
-        } 
+        }
         max = sum;
         for(int i = k; i < n; i++) {
-            int ne = nums[i];
-            int oe = nums[i-k];
-            sum = sum + ne - oe;
+            sum = sum + nums[i] - nums[i-k];
             max = Math.max(sum,max);
         }
         return (double) max / k;
