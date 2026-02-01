623ecd0badbdfa9e1c5691172f7c332ec8650da1
nidhi <nidhidevulapally20@gmail.com>
2026-02-01 13:17:15 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (84.15%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.java
@@ -2,7 +2,7 @@ class Solution {
     public int countSeniors(String[] details) {
         int cnt = 0;
         for(String s: details) {
-            if(Integer.parseInt(s.substring(11, 13)) > 60) cnt++;
+            if((s.charAt(11) - '0') * 10 + s.charAt(12) - '0' > 60) cnt++;
         }
         return cnt;
     }
