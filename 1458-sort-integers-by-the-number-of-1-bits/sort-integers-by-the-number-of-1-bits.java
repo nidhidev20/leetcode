05db24a04a40fa38a84ca962224d6574bf7f43d9
nidhi <nidhidevulapally20@gmail.com>
2026-02-26 19:22:04 +0530

    Time: 8 ms (61.44%) | Memory: 46.3 MB (96.43%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -2,7 +2,8 @@ class Solution {
     public int[] sortByBits(int[] arr) {
         
         Integer[] ans = new Integer[arr.length];
-        for(int i = 0; i < arr.length; i++) {
+        int n = arr.length;
+        for(int i = 0; i < n; i++) {
             ans[i] = arr[i];
         }
 
@@ -13,7 +14,7 @@ class Solution {
             if(bitsa == bitsb) return a - b;
             return bitsa - bitsb;
         });
-        for(int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < n; i++) {
             arr[i] = ans[i];
         }
         return arr;
